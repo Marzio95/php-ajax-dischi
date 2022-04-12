490ba19a11be6c1424350306282fbf35ee1c0fbe
Marzio95 <marziodellarocca14@gmail.com>
2022-04-12 17:13:45 +0200

    implemento la funzione di cambio di genere con il selected in vue

--- frontend/script.js
+++ frontend/script.js
@@ -2,13 +2,31 @@ new Vue({
     el: '#app',
     data() {
         return {
-            arrayDischi: ''
+            arrayDischi: '',
+            selected: "general",
         }
 
     },
     created() {
         axios.get('http://localhost/php-ajax-dischi/db/data.php')
             .then(response => this.arrayDischi = response.data)
+    },
+    computed: {
+        changeMusicFunction() {
+            let arrayDischiFiltered = null;
+            if (this.selected.toLowerCase() == "") {
+                return this.arrayDischi;
+            } else if (this.selected.toLowerCase() == "general") {
+                return this.arrayDischi;
+            } else {
+                arrayDischiFiltered = this.arrayDischi.filter((element) => {
+                    return (
+                        element.genre.toLowerCase() == this.selected.toLowerCase()
+                    );
+                });
+            }
 
+            return arrayDischiFiltered;
+        },
     }
 })
